b6c1ed3a98089048ad97af81a36d4e0aaaf7ef7c
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-10-19 20:38:08 +0300

    add sum

--- js/array.js
+++ js/array.js
@@ -34,3 +34,23 @@ let gameUHD = [
 
 
 
+// var arr = [3,2,5,6];
+// function arraySum(array){
+// var sum = 0;
+// for(var i = 0; i < array.length; i++){
+//     sum += array[i];
+// }
+// console.log(sum);
+// }
+// arraySum(arr);
+
+// const arr = [5, 8, 7];
+// function getSum(array) {
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         sum += array[i];
+//     }
+
+//     console.log(sum);
+// }
+// getSum(arr);
